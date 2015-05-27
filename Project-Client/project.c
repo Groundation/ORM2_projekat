a2acb4eb3c354d4a45a08678ad85a764c1b3add3
Groundation <nenadpekez@live.com>
2015-05-27 23:26:31 +0200

    Promene u clientu i dodati odredjeni fajlovi

--- Project-Client/project.c
+++ Project-Client/project.c
@@ -11,70 +11,20 @@
 #endif
 
 #include <pcap.h>
- 
-#define DATA_SIZE 8
-
-/* Packet header and data */
-typedef struct pkt_data
-{
-	u_int seq;
-	u_int ack;
-	char data[DATA_SIZE];
-}pkt_data;
-
-/* 4 bytes IP address */
-typedef struct ip_address
-{
-	u_char byte1;
-	u_char byte2;
-	u_char byte3;
-	u_char byte4;
-}ip_address;
-
-/* IPv4 header */
-typedef struct ip_header
-{
-	u_char	ver_ihl;		// Version (4 bits) + Internet header length (4 bits)
-	u_char	tos;			// Type of service 
-	u_short tlen;			// Total length 
-	u_short identification; // Identification
-	u_short flags_fo;		// Flags (3 bits) + Fragment offset (13 bits)
-	u_char	ttl;			// Time to live
-	u_char	proto;			// Protocol
-	u_short crc;			// Header checksum
-	ip_address	saddr;		// Source address
-	ip_address	daddr;		// Destination address
-}ip_header;
-
-/* UDP header*/
-typedef struct udp_header
-{
-	u_short sport;			// Source port
-	u_short dport;			// Destination port
-	u_short len;			// Datagram length
-	u_short crc;			// Checksum
-}udp_header;
-
-/* Ethernet header */
-typedef struct eth_header
-{
-	u_char dest[6];
-	u_char source[6];
-	u_char eth_type[2];
-}eth_header;
+#include <pthread.h>
+#include "orm_types.h"
 
 int packet_size = sizeof(eth_header) + sizeof(udp_header)+sizeof(ip_header)+sizeof(pkt_data); 
 
-void PreparePacket(u_char **packet, char *data)
+void PreparePacket(u_char *packet, char *data)
 {
 	eth_header* eh;
 	ip_header* ih;
 	udp_header* uh;
 	pkt_data* pd;
 
-	*packet = (u_char*) malloc(packet_size);
 	/*Ethernet header*/
-	eh = (eth_header*) *packet;
+	eh = (eth_header*) packet;
 	eh->source[0] = 0x00;
 	eh->source[1] = 0x19;
 	eh->source[2] = 0x99;
@@ -90,7 +40,7 @@ void PreparePacket(u_char **packet, char *data)
 	eh->eth_type[0] = 0x08;
 	eh->eth_type[1] = 0x00;
 	/*IP header*/
-	ih = (ip_header *) (*packet + sizeof(eth_header));
+	ih = (ip_header *) (packet + sizeof(eth_header));
 	ih->ver_ihl = 0x45;									// Version (4 bits) + Internet header length (4 bits)
 	ih->tos = 0;										// Type of service 
 	ih->tlen = htons(packet_size - sizeof(eth_header));	// Total length
@@ -107,15 +57,15 @@ void PreparePacket(u_char **packet, char *data)
 	ih->daddr.byte3 = 32;
 	ih->daddr.byte4 = 52;
 	/*UDP header*/
-	uh = (udp_header*) (*packet + sizeof(eth_header) + sizeof(ip_header));
+	uh = (udp_header*) (packet + sizeof(eth_header) + sizeof(ip_header));
 	uh->dport = htons(50030);
 	uh->sport = htons(50030);
 	uh->len = htons(sizeof(udp_header) + sizeof(pkt_data));
 	/*Packet Data*/
-	pd = (pkt_data*) (*packet + sizeof(eth_header) + sizeof(ip_header) + sizeof(udp_header));
+	pd = (pkt_data*) (packet + sizeof(eth_header) + sizeof(ip_header) + sizeof(udp_header));
 	pd->seq = 1;
 	pd->ack = 2;
-	strcpy(pd->data,data);
+	memcpy(pd->data, data, DATA_SIZE); //PROMENJENA LINIJA KODA!!! stajalo: strcpy
 }
 
 int main()
@@ -123,18 +73,6 @@ int main()
 	pcap_if_t *alldevs; 
 	pcap_if_t *d;
 	int n;
-
-	/**
-	pcap_if_t -> instance of pcap_if structure; fields :
-	{
-		struct pcap_if *next;			// pointer to next element in list
-		char *name;						// name to hand to "pcap_open_live()" 
-		char *description;				//textual description to user interface
-		struct pcap_addr *addresses;	//pointer, to the 1st el. of list of addresses for interface
-		bpf_u_int32 flags;				// PCAP_IF_ interface flags
-	}
-	
-	**/
 
 	int inum;
 	int i=0;
@@ -145,9 +83,10 @@ int main()
 	u_int netmask;
 	char packet_filter[] = "ip and udp and dst port 50030";
 	struct bpf_program fcode;
-	u_char* buf;
-	char data[8];
-	strcpy(data, "test123");
+	
+	FILE *ptr_myfile;
+	u_char buf[sizeof(eth_header) + sizeof(udp_header)+sizeof(ip_header)+sizeof(pkt_data)];
+	char read_data[DATA_SIZE];
 	
 	/* Retrieve the device list */
 	if(pcap_findalldevs(&alldevs, errbuf) == -1)
@@ -203,11 +142,25 @@ int main()
 		return -1;
 	}
 	
-	PreparePacket(&buf,data);
-	if (pcap_sendpacket(adhandle, buf, packet_size) != 0)
-    {
-        fprintf(stderr,"\nError sending the packet: %s\n", pcap_geterr(adhandle));
-        return;
-    }
+	ptr_myfile = fopen("test.txt","rb");
+	if (!ptr_myfile)
+	{
+		printf("Unable to open file!");
+		return 1;
+	}
+
+	for(i=0; i<3; i++)
+	{
+		fread(read_data, DATA_SIZE, 1, ptr_myfile);
+		PreparePacket(buf, read_data);
+		if (pcap_sendpacket(adhandle, buf, packet_size) != 0)
+		{
+			fprintf(stderr,"\nError sending the packet: %s\n", pcap_geterr(adhandle));
+			return;
+		}
+	}
+	
+	fclose(ptr_myfile);
+
 	return 0;
 }
