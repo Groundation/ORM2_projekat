404450eaa399fef2c7e91175166628985f1d551c
Sasa Talosi <jasaleja@gmail.com>
2015-05-28 23:41:28 +0200

    Promene u projektu servera
    Napravljeno da pthreadVC2 bude globalan za oba projekta!

--- Project-Server/project.c
+++ Project-Server/project.c
@@ -2,17 +2,9 @@
 // Osnovi Racunarskih Mreza 2
 // File name: project.c
 
-#ifdef _MSC_VER
-/*
- * we do not want the warnings about the old deprecated and unsecure CRT functions
- * since these examples can be compiled under *nix as well
- */
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
 #include "orm_types.h"
-#include "pthreadVC2\pthread.h"
-#include "pthreadVC2\semaphore.h"
+#include "pthread.h"
+#include "semaphore.h"
 #include "global.h"
 #include "proto.h"
 #include "fun.c"
